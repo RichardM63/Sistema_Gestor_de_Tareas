78b4df5021ba746f6aae8f4735205824bad53786
Londrina63 <122219073+RichardM63@users.noreply.github.com>
2025-04-14 05:40:43 -0500

    Update AdminController.java

--- src/main/java/com/gestor/de/tareas/gestor/de/tareas/controller/admin/AdminController.java
+++ src/main/java/com/gestor/de/tareas/gestor/de/tareas/controller/admin/AdminController.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.security.Principal;
 import java.util.ArrayList;
